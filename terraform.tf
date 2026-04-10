4a89898b9fe3ebf702b8fb462533dc80286acaac
Devaraj <sharon.devaraj@accenture.com>
2026-04-10 12:23:50 +0530

    initial code

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "demo-bucket-four" {
   name          = "cruk-bucket-four"
   location      = "US"
+  project       = "do-not-delete-444606"
   uniform_bucket_level_access = true
   }
