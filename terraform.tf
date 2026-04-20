4643560636f40c5dbce24fd272a462739b6c5f7f
Devaraj <sharon.devaraj@accenture.com>
2026-04-20 10:28:28 +0530

    changes updated

--- terraform.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "demo-bucket-four" {
-  name                        = "cruk-bucket-four"
-  location                    = "US"
-  project                     = "do-not-delete-444606"
-  uniform_bucket_level_access = true
-}
